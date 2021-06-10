c3416e574f9f1cd1fc9570bd1bc46daa09789fea
cyr <2870894168@qq.com>
2021-06-10 14:47:37 +0800

    update memberMain

--- shop-member-api/src/main/java/com/fh/shop/ShopMemberApp.java
+++ shop-member-api/src/main/java/com/fh/shop/ShopMemberApp.java
@@ -10,7 +10,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ShopMemberApp {
 
     public static void main(String[] args) {
-        System.out.println("今天学习了git，真的好开心");
         SpringApplication.run(ShopMemberApp.class,args);
     }
 }
